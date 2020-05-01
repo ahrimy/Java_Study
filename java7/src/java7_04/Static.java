d322f1c9471c590f743d5351a9458f69ea42d017
ahrimy <dlsdjdkfla@gmail.com>
2020-05-01 15:31:10 +0900

    #Static(전역 변수)
    //static // 전역변수 
    // 프로그램이 시작할때 할당받아서 
    // 프로그램이 종료될때 해제되는 변수 (위치 : static 영역)

---
+++ java7/src/java7_04/Static.java
@@ -0,0 +1,20 @@
+package java7_04;
+class Test{
+	int a;
+	static int b;
+	int c;
+	static void test() {		
+	}
+}
+
+public class Static {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test.b = 100;
+		Test t = new Test();
+		Test t1 = new Test();
+
+	}
+
+}
